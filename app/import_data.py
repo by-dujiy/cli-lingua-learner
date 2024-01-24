9f4b75bed74ad6810d341b54ccf97a07182cdd45
by-dujiy <bydujiy@gmail.com>
2024-01-24 18:06:42 +0200

    upd models - added Collection and one-to-many relations

---
+++ app/import_data.py
@@ -0,0 +1,3 @@
+from gs_reader import GSClientReader
+from db import Model, Session, engine
+from models import WordsPair, Translation
--- app/models.py
+++ app/models.py
@@ -1,24 +1,49 @@
 from db import Model
-from sqlalchemy import String, Integer, ForeignKey
+from sqlalchemy import String, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 
-class WordsPair(Model):
-    __tablename__ = 'wordspair'
+class Collection(Model):
+    __tablename__ = 'collections'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    word: Mapped[str] = mapped_column(String(64), nullable=False)
+    name: Mapped[str] = mapped_column(String(64), index=True, uique=True)
 
-    translations = relationship('Translation', back_populates='word_pair')
+    wordspairs: Mapped[list['WordsPair']] = relationship(
+        cascade='all, delete-orphan', back_populates='collection')
+
+    def __repr__(self):
+        return f"Collection({self.id}, '{self.name}')"
+
+
+class WordsPair(Model):
+    __tablename__ = 'wordspairs'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    word: Mapped[str] = mapped_column(String(64), unique=True)
+    collection_id: Mapped[int] = mapped_column(
+        ForeignKey('collections.id'), index=True)
+
+    collection: Mapped['Collection'] = relationship(
+        back_populates='wordspairs')
+
+    translations: Mapped[list['Translation']] = relationship(
+        cascade='all, delete-orphan', back_populates='wordspair')
+
+    def __repr__(self):
+        return f"WordsPair({self.id}, '{self.word}')"
 
 
 class Translation(Model):
-    __tablename__ = 'translation'
+    __tablename__ = 'translations'
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True)
     translation_word: Mapped[str] = mapped_column(String(64), nullable=False)
-    word_pair_id: Mapped[int] = mapped_column(Integer,
-                                              ForeignKey('wordspair.id'),
-                                              nullable=False)
+    wordspair_id: Mapped[int] = mapped_column(
+        ForeignKey('wordspairs.id'), index=True)
 
-    word_pair = relationship("WordsPair", back_populates='translations')
+    wordspair: Mapped['WordsPair'] = relationship(
+        back_populates='translations')
+
+    def __repr__(self):
+        return f"Translation({self.id}, '{self.translation_word}')"
